af0ebf6f6e5051b4261442fc234ea2b8d15f994d
Philip Orvis <orvisuals@gmail.com>
2020-04-26 09:56:12 -0500

    display results function creted

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
         //$('.resultSearch').emnpty();
-        //$('.js-error-message').empty();
+        $('.js-error-message').empty();
         var stateVal = document.getElementsByClassName("state")[0].value;
         console.log(stateVal);
+        getWeatherAlert(stateVal);
     })
 }
 
-function getWeatherAlert(StateVal) {
+function getWeatherAlert(stateVal) {
     var baseUrl = "https://api.weather.gov/alerts/active/area/";
     fetch (`${baseUrl}/${stateVal}`)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => $('.js-error-message').text(`Something went wrong: ${error.message}`)
+    )};
+
+function displayResults(responseJson) {
+    console.log(responseJson);
 }
 
 
