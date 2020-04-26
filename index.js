c58e2bc1db1b18ff20a7aad75fdbc1e6d3863c13
Philip Orvis <orvisuals@gmail.com>
2020-04-26 12:56:05 -0500

    created getshelter function, has issues with cors. Asking mentor today

--- index.js
+++ index.js
@@ -23,13 +23,35 @@ function getLatLong(stateVal, addressVal) {
         }
         throw new Error(response.statusText);
     })
-    .then(responseJson => getShelterLocation(responseJson))
+    .then(geoData => getShelterLocation(geoData))
     .catch(error => $('.js-error-message').text(`Something went wrong: ${error.message}`)
     )};
 
-    function getShelterLocation(responseJson) {
-        console.log(responseJson);
-    }
+    function getShelterLocation(geoData) {
+        console.log(geoData);
+        var results = Object.values(geoData)[0][0];
+        var latLong = results.geometry.location;
+        var latLongVal = Object.values(latLong).toString();
+        var baseUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?`;
+        var key="&key=AIzaSyBlqsxvTm23APcJur8ztY7Ul_4Bdl5Czjs";
+        console.log(latLongVal.toString());
+        fetch (`${baseUrl}input=emergency+shelter&inputtype=textquery&locationbias=circle:5000@${latLongVal}${key}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResultsShelter(responseJson))
+        .catch(error => $('.js-error-message').text(`Something went wrong: ${error.message}`)
+        )};
+    
+
+function displayResultsShelter(responseJson) {
+            console.log(responseJson);
+}
+        
+
 
 function getWeatherAlert(stateVal) {
     var baseUrl = "https://api.weather.gov/alerts/active/area/";
